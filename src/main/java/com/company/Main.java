361889d9bdd5c2f61f916b911541aea71a57e7a4
Timur Tasci <timurtasci@gmail.com>
2016-01-12 16:28:06 +0100

    finished sample client without service discovery

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -8,29 +8,15 @@ import java.util.concurrent.TimeoutException;
 public class Main {
 
     private static final String EXCHANGE_NAME = "Demo.Dynamic.Scalar.UInt16";
-    public static void main(String[] args) throws IOException {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        Connection connection = null;
-        try {
-            connection = factory.newConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        }
-        Channel channel = connection != null ? connection.createChannel() : null;
+    public static void main(String[] args) throws Exception {
 
-        if (channel != null) {
-            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
-        }
-        assert channel != null;
-        String queueName = channel.queueDeclare().getQueue();
-        channel.queueBind(queueName, EXCHANGE_NAME, "");
+        SubscribeManager subscribeManager = SubscribeManager.getInstance();
+        subscribeManager.init("localhost");
+        subscribeManager.createChannel(EXCHANGE_NAME, "fanout");
 
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        Consumer consumer = new DefaultConsumer(channel) {
+        Consumer consumer = new DefaultConsumer(subscribeManager.getChannelFromExchange(EXCHANGE_NAME)) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope,
                                        AMQP.BasicProperties properties, byte[] body) throws IOException {
@@ -38,7 +24,7 @@ public class Main {
                 System.out.println(" [x] Received '" + message + "'");
             }
         };
-        channel.basicConsume(queueName, true, consumer);
+        subscribeManager.getChannelFromExchange(EXCHANGE_NAME).basicConsume(subscribeManager.bindToQueue(EXCHANGE_NAME), true, consumer);
     }
 
 }
